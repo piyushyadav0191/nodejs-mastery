95cd52aba6186e171f24e6631494e235a842085b
Piyush Yadav <piyushyadav0191@gmail.com>
2023-02-08 20:17:39 +0530

    second complete

--- data.js
+++
@@ -1,7 +0,0 @@
-const data = [
-  { name: "Piyush", email: "piyush@gmail.com" },
-  { name: "damn", email: "damn@gmail.com" },
-  { name: "cool", email: "cool@gmail.com" },
-];
-
-module.exports = data;
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const http = require("http");
-const data = require("./data");
+// console.log(process.argv[2]); // node index.js helloPiyush
+const fs = require("fs");
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(data));
-    res.end("Hello world!"); // after end we cant write any more data
-  })
-  .listen(5000);
+const input = process.argv;
 
-// What we convered in this
-// Make a server, create headers and body , create api with static data,
-// put data in another file
+if (input[2] === "add") {
+  fs.writeFileSync(input[3], input[4]); // de index.js add testing1.txt 'This is our test 1 for adding'
+} else if (input[2] === "remove") {
+  fs.unlinkSync(input[3]); // node index.js add testing1.txt 'This is our test 1 for adding'
+} else console.log("Invalid input");
+
+// why 2 3 right?
+// because , node index.js hello
+// [
+//  0  '/usr/bin/node',
+//  1  '/home/piyush/Desktop/nodejs-tutorial/index.js',
+//  2 'hello'
+// ]
+// so our file write at 2nd index so file name then content in file
+
+// What we covered in this
+// set input from command line, create file with input, delete file with input
