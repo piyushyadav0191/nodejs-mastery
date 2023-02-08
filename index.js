cf6898339452024d8daa411cee3196a54850a7d8
Piyush Yadav <piyushyadav0191@gmail.com>
2023-02-08 20:42:33 +0530

    third complete

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-// console.log(process.argv[2]); // node index.js helloPiyush
 const fs = require("fs");
+const path = require("path");
+const dirPath = path.join(__dirname, "files");
+// console.log(dirPath); /home/piyush/Desktop/nodejs-tutorial/files
 
-const input = process.argv;
+for (let i = 0; i < 5; i++) {
+  fs.writeFileSync(`${dirPath}/hello${i}.txt`, "Aweosme file created ");
+}
 
-if (input[2] === "add") {
-  fs.writeFileSync(input[3], input[4]); // de index.js add testing1.txt 'This is our test 1 for adding'
-} else if (input[2] === "remove") {
-  fs.unlinkSync(input[3]); // node index.js add testing1.txt 'This is our test 1 for adding'
-} else console.log("Invalid input");
-
-// why 2 3 right?
-// because , node index.js hello
-// [
-//  0  '/usr/bin/node',
-//  1  '/home/piyush/Desktop/nodejs-tutorial/index.js',
-//  2 'hello'
-// ]
-// so our file write at 2nd index so file name then content in file
-
-// What we covered in this
-// set input from command line, create file with input, delete file with input
+fs.readdir(dirPath, (err, files) => {
+  files.forEach((item) => {
+    console.log(`file name is ${item}`);
+  });
+});
+// what we covered
+//make file in folder,use path module , get files name and print
